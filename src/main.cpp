3f14e059b6ef14772fe2146c1c31e9e5cdf60cd9
ErnestoUrrea <45474433+ErnestoUrrea@users.noreply.github.com>
2023-09-27 01:42:47 -0600

    Implemented PID control cycle

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,40 @@
 #include "adc.h"
 
 volatile uint8_t flag = 0;
-uint16_t ADC_value;
 
+#define VR 5.0
+
+volatile uint16_t sensorValue = 0;          // raw sensor reading
+float sensorVoltage = 0;      // sensor voltage reading
+ 
+// control parameters
+float desiredVoltage = 2.5;   // desired position of the ball
+
+// control variables
+float errorK;                 // position error at the time instant k
+float errorKm1 = 0;             // position error at the time instant k-1
+float errorKm2 = 0;             // position error at the time instant k-2
+float controlK = 0;             // control signal at the time instant k
+float controlKm1 = 0;           // control signal at the time instant k-1
+ 
+// control constants
+float Kp = 0.2;               // proportional control 
+float Ki = 10;                // integral control
+float Kd = 0.4;               // derivative control
+float h = 1/20000;            // discretization constant                        
+ 
+float keK = Kp*(1+h/Ki+Kd/h);       // parameter that multiplies the error at the time instant k
+float keKm1 = -Kp*(1+2*Kd/h);       // parameter that multiplies the error at the time instant k-1                    
+float keKm2 = Kp*Kd/h;              // parameter that multiplies the error at the time instant k-2
+ 
 // ADC Interrupt Service Routine
 ISR(ADC_vect) {
-  // Read ADC Value
-  ADC_value = ADC;
-  // Set Flag for ADC Complete
-  flag = 1;
   // Clear Timer Compare Match Flag A
   TIFR0 = (1 << OCF0A);
+  // Read ADC Value
+  sensorValue = ADC;
+  // Set Flag for ADC Complete
+  flag = 1;
 }
 
 int main(void){
@@ -32,7 +56,7 @@ int main(void){
   StartTimer1(0b001); // CS
 
   // Initialize ADC
-  InitADC(0b01, 0b0, 0b0000, 0b001); // REFS, LAR, MUX, PRE
+  InitADC(0b01, 0b0, 0b0000, 0b101); // REFS, LAR, MUX, PRE
 
   // ! Pending: Modify SetADCChannel Function to Disable Digital Input Buffers
   DIDR0 = 0b00000001;
@@ -45,15 +69,25 @@ int main(void){
 
   while (1) {
     if (flag) {
-      // Clear global interrupts
-      cli();
-      // Enable global interrupts
-      sei();
-      // Clear ADC Complete Flag
+      // clear the new data flag
       flag = 0;
-      // TODO: PID Controller 
-      // Set PWM Duty Cycle depending on ADC reading (5 to 95%)
-      OCR1A = (ADC_value/1024.00)*720 + 40;
+
+      // sensor reading in volts
+      sensorVoltage = sensorValue*VR/1024;
+    
+      // error at the time instant k;
+      errorK = desiredVoltage - sensorVoltage;
+    
+      // compute the control signal
+      controlK = controlKm1 + keK*errorK + keKm1*errorKm1 + keKm2*errorKm2;
+    
+      // update the values for the next iteration
+      controlKm1 = controlK;
+      errorKm2 = errorKm1;
+      errorKm1 = errorK;
+    
+      // set pwm duty cycle
+      OCR1A = (controlK/5.0)*720 + 360 + 40; // the number 94 is the control action necessary to keep the ball in the horizontal position
     }
   }
 }
